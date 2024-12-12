2b70cda00894fd8c9e1905601cf7d5f0a5d26690
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2024-12-12 17:38:47 -0500

    Add: functions anony

--- platzi/Object Oriented Programming and Concurrency/4-factory/main.go
+++ platzi/Object Oriented Programming and Concurrency/4-factory/main.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-type INotification interface {
+// SMS Email
+
+type INotificationFactory interface {
 	SendNotification()
 	GetSender() ISender
 }
@@ -12,28 +14,77 @@ type ISender interface {
 	GetSenderChannel() string
 }
 
-
 type SMSNotification struct {
-
 }
 
-func (n *SMSNotification) SendNotification() {
-  fmt.Println("Sending SMS notification...")
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending Notification via SMS")
 }
 
-func (n *SMSNotification) GetSender() ISender {
-  return &SMSNotificationSender{}
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
 }
 
 type SMSNotificationSender struct {
-
 }
 
-func (s *SMSNotificationSender) GetSenderMethod() string {
-  return "SMS"
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
 }
 
-func (s *SMSNotificationSender) GetSenderChannel() string {
-  return "SMS Channel"
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
 }
 
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("Sending Notification via Email")
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "SES"
+}
+
+func getNotificationFactory(notificationType string) (INotificationFactory, error) {
+	if notificationType == "SMS" {
+		return &SMSNotification{}, nil
+	}
+
+	if notificationType == "Email" {
+		return &EmailNotification{}, nil
+	}
+
+	return nil, fmt.Errorf("No Notification Type")
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+
+	getMethod(smsFactory)
+	getMethod(emailFactory)
+}
---
+++ platzi/Object Oriented Programming and Concurrency/6-variadic-functions.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+func sum(values ...int) int {
+	total := 0
+	for _, value := range values {
+    total += value
+  }
+	return total
+}
+
+func getValues(x int) (int, int , int)  {
+	return 2 * x, 2 * x, 2 * x
+}
+
+func getValue(x int) (single int , doblue int , div int ) {
+	single = x
+	doblue = 2 * x
+	div = x / 2
+	return
+}
+
+func main() {
+	fmt.Print(sum(11))
+	fmt.Print("\n")
+	fmt.Print(sum(1, 2, 3, 4, 5))
+	fmt.Print("\n")
+	fmt.Print(getValues(2))
+	fmt.Print("\n")
+	single, doblue, div := getValue(3)
+	fmt.Print(single, doblue, div)
+}
